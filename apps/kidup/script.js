e4576eb9f4b92ca9958a1b2b586c70bc38ed4680
sexyflash <sexyflash@gmail.com>
2025-03-20 00:30:04 +0900

    Add files via upload

---
+++ apps/kidup/script.js
@@ -0,0 +1,8 @@
+/* script.js */
+document.addEventListener("DOMContentLoaded", function() {
+    const ctaButton = document.querySelector(".cta-button");
+    ctaButton.addEventListener("click", function() {
+        alert("알림이 등록되었습니다!");
+    });
+});
+
